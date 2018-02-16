dab7f12ed662bd62c846baa676994f640bfcd4f4
randomname8 <rpiscore@gmail.com>
2018-02-16 19:14:33 -0300

    take parameters like the image does

--- src/main/scala/ccc/util/WeakImage.scala
+++ src/main/scala/ccc/util/WeakImage.scala
@@ -9,10 +9,13 @@ import scala.ref.WeakReference
  * If the image is requested after it was collected, it is re-retrieved, this allow for efficent caching of images while specially keeping
  * the visible ones, and allowing the rest to be collected.
  */
-class WeakImage(imageLocation: String) {
+class WeakImage(val imageLocation: String,
+                val requestedWidth: Double = 0, val requestedHeight: Double = 0,
+                val preserveRatio: Boolean = true, val smooth: Boolean = true,
+                val backgroundLoading: Boolean = true) {
   private[this] var imageReference = WeakReference[Image](null)
   def get: Image = imageReference.get.getOrElse {
-    val res = new Image(imageLocation, true)
+    val res = new Image(imageLocation, requestedWidth, requestedHeight, preserveRatio, smooth, backgroundLoading)
     imageReference = WeakReference(res)
     res
   }
